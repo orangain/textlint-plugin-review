56cb5dfb35ba621b2d0224a2a5013c2d4b36cbb2
orangain <orangain@gmail.com>
2016-05-22 00:06:51 +0900

    Update textlint to 6.8.0

--- test/ReVIEWProcessor-test.js
+++ test/ReVIEWProcessor-test.js
@@ -18,10 +18,11 @@ describe('ReVIEWProcessor', function () {
       });
 
       it('should report error', function () {
-        var fixturePath = path.join(__dirname, '/fixtures/test.re');
-        let results = textlint.lintFile(fixturePath);
-        assert(results.messages.length > 0);
-        assert(results.filePath === fixturePath);
+        const fixturePath = path.join(__dirname, '/fixtures/test.re');
+        return textlint.lintFile(fixturePath).then(results => {
+          assert(results.messages.length > 0);
+          assert(results.filePath === fixturePath);
+        });
       });
     });
   });
